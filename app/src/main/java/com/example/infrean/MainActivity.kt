86aeaf4aaacb3b86d034258939aa4f402085027a
KUD <kimud6003@gmail.com>
2021-02-17 18:06:44 +0900

    JetPack2_ViewModel

--- app/src/main/java/com/example/infrean/MainActivity.kt
+++ app/src/main/java/com/example/infrean/MainActivity.kt
@@ -2,25 +2,30 @@ package com.example.infrean
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database-name"
-        ).allowMainThreadQueries().build()
-        db.todoDao().getAll().observe(this, Observer { todos ->
+        val viewModel: MainViewModel by viewModels()
+
+        viewModel.getAll().observe(this, Observer { todos ->
             result_text.text = todos.toString()
         })
 
         add_Button.setOnClickListener{
-            db.todoDao().insert(Todo(todo_Edit.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO){
+                viewModel.insert(Todo(todo_Edit.text.toString()))
+            }
         }
     }
 }
