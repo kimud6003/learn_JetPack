3f936e8fc0964e8ad28aac8483b3cc286621ea88
KUD <kimud6003@gmail.com>
2021-02-17 22:34:50 +0900

    JetPack2_Navigations

--- app/src/main/java/com/example/infrean/MainActivity.kt
+++ app/src/main/java/com/example/infrean/MainActivity.kt
@@ -4,13 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
 import com.example.infrean.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
