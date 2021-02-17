4103a74a5e57c4af01a2b840a6946b55ee1b1edd
KUD <kimud6003@gmail.com>
2021-02-17 17:35:13 +0900

    JetPack1_RoomDatabase

---
+++ app/src/main/java/com/example/infrean/Todo.kt
@@ -0,0 +1,12 @@
+package com.example.infrean
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo (
+    var title: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
